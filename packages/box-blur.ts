e507f80381ea2f252745d932f5ec83d43c2f2fdf
GeonHyeokLee <geonhyeoklee.kr@gmail.com>
2024-06-02 10:47:07 +0900

    change: separate core

--- packages/box-blur.ts
+++ packages/box-blur.ts
@@ -1,126 +1,85 @@
-import {
-  get2DCanvas,
-  getPixel,
-  loadImage,
-  clamp,
-  removeDecimalPoint,
-} from "./utils";
-import { Pixel } from "./types/pixel";
+import { getPixel, removeDecimalPoint } from "./utils";
+import { baseBlur } from "./core";
+import { Pixel, RGBA } from "./types/pixel";
 
-const INTERPOLATION = 4;
-
-function convertImageDataToPixelsBuffer(imageData: Uint8ClampedArray): Pixel[] {
-  const pixelsBuffer: Pixel[] = [];
-  let pixelBuffer: number[] = [];
-  for (const data of imageData) {
-    pixelBuffer.push(data);
-
-    if (pixelBuffer.length === INTERPOLATION) {
-      pixelsBuffer.push(pixelBuffer as Pixel);
-      pixelBuffer = [];
-    }
-  }
-
-  return pixelsBuffer;
-}
-
-function convertPixelsBufferToImageData(
-  pixelsBuffer: Pixel[],
-  width: number,
-  height: number
-) {
-  return new ImageData(
-    new Uint8ClampedArray(pixelsBuffer.flat()),
-    width,
-    height
-  );
+export async function boxBlur(imageUrl: string): Promise<ImageData> {
+  return await baseBlur(imageUrl, calculateBoxBlurPixelsBuffer);
 }
 
 function calculateBoxBlurPixelsBuffer(
   pixelsBuffer: Pixel[],
-  intensity: number,
   width: number,
   height: number
 ): void {
-  const MAX_NEIGHBOR_COUNT = 5;
-  const MIDDLE_NEIGHBOR_INDEX = Math.floor(MAX_NEIGHBOR_COUNT / 2);
+  const MAX_NEIGHBOR = 5;
+  const MIDDLE_NEIGHBOR = Math.floor(MAX_NEIGHBOR / 2);
+  const INTENSITY = 1 / MAX_NEIGHBOR;
 
   // TODO: 시간 복잡도 개선 필요
   // 시간 복잡도 O(n^3), 공간 복잡도 O(n)
+  // X축 적용
   for (let j = 0; j < height; j++) {
     for (let i = 0; i < width; i++) {
       const neighborPixelSum: Pixel = [0, 0, 0, 255];
 
-      for (let si = 0; si < MAX_NEIGHBOR_COUNT; si++) {
+      for (let si = 0; si < MAX_NEIGHBOR; si++) {
         const neighborPixel = getPixel(
           pixelsBuffer,
           width,
           height,
-          i + si - MIDDLE_NEIGHBOR_INDEX,
+          i + si - MIDDLE_NEIGHBOR,
           j
         );
-        neighborPixelSum[0] += neighborPixel[0];
-        neighborPixelSum[1] += neighborPixel[1];
-        neighborPixelSum[2] += neighborPixel[2];
+
+        neighborPixelSum[RGBA.Red] += neighborPixel[RGBA.Red];
+        neighborPixelSum[RGBA.Green] += neighborPixel[RGBA.Green];
+        neighborPixelSum[RGBA.Blue] += neighborPixel[RGBA.Blue];
       }
 
       const targetPixelIndex = i + width * j;
 
-      pixelsBuffer[targetPixelIndex][0] = removeDecimalPoint(
-        neighborPixelSum[0] * intensity
+      pixelsBuffer[targetPixelIndex][RGBA.Red] = removeDecimalPoint(
+        neighborPixelSum[RGBA.Red] * INTENSITY
       );
-      pixelsBuffer[targetPixelIndex][1] = removeDecimalPoint(
-        neighborPixelSum[1] * intensity
+      pixelsBuffer[targetPixelIndex][RGBA.Green] = removeDecimalPoint(
+        neighborPixelSum[RGBA.Green] * INTENSITY
       );
-      pixelsBuffer[targetPixelIndex][2] = removeDecimalPoint(
-        neighborPixelSum[2] * intensity
+      pixelsBuffer[targetPixelIndex][RGBA.Blue] = removeDecimalPoint(
+        neighborPixelSum[RGBA.Blue] * INTENSITY
+      );
+    }
+  }
+
+  // Y축 적용
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++) {
+      const neighborPixelSum: Pixel = [0, 0, 0, 255];
+
+      for (let si = 0; si < MAX_NEIGHBOR; si++) {
+        const neighborPixel = getPixel(
+          pixelsBuffer,
+          width,
+          height,
+          i,
+          j + si - MIDDLE_NEIGHBOR
+        );
+
+        neighborPixelSum[RGBA.Red] += neighborPixel[RGBA.Red];
+        neighborPixelSum[RGBA.Green] += neighborPixel[RGBA.Green];
+        neighborPixelSum[RGBA.Blue] += neighborPixel[RGBA.Blue];
+      }
+
+      const targetPixelIndex = i + width * j;
+
+      pixelsBuffer[targetPixelIndex][RGBA.Red] = removeDecimalPoint(
+        neighborPixelSum[RGBA.Red] * INTENSITY
+      );
+      pixelsBuffer[targetPixelIndex][RGBA.Green] = removeDecimalPoint(
+        neighborPixelSum[RGBA.Green] * INTENSITY
+      );
+      pixelsBuffer[targetPixelIndex][RGBA.Blue] = removeDecimalPoint(
+        neighborPixelSum[RGBA.Blue] * INTENSITY
       );
     }
   }
 }
-
-function getBlurImageData(
-  imageData: Uint8ClampedArray,
-  width: number,
-  height: number,
-  intensity?: number
-): ImageData {
-  if (imageData.length % INTERPOLATION !== 0) {
-    throw new Error("It is Wrong image data.");
-  }
-
-  if (imageData.length !== width * height * INTERPOLATION) {
-    throw new Error("It is Wrong image data.");
-  }
-
-  const pixelsBuffer = convertImageDataToPixelsBuffer(imageData);
-  const clampedIntensity = clamp(intensity ?? 0.2, 0, 1);
-
-  calculateBoxBlurPixelsBuffer(pixelsBuffer, clampedIntensity, width, height);
-
-  return convertPixelsBufferToImageData(pixelsBuffer, width, height);
-}
-
-async function getOriginalImageData(imageUrl: string) {
-  const { image, naturalWidth, naturalHeight } = await loadImage(imageUrl);
-  const { ctx } = get2DCanvas();
-  const dw = naturalWidth;
-  const dh = naturalHeight;
-  ctx.drawImage(image, 0, 0, dw, dh);
-
-  return {
-    imageData: ctx.getImageData(0, 0, dw, dh).data,
-    width: dw,
-    height: dh,
-  };
-}
-
-async function boxBlur(
-  imageUrl: string,
-  option?: { intensity?: number }
-): Promise<ImageData> {
-  const { imageData, width, height } = await getOriginalImageData(imageUrl);
-  return getBlurImageData(imageData, width, height, option?.intensity);
-}
-
-export { boxBlur };
